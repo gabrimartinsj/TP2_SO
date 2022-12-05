fa444f78aa6ab8a272518c977f4d2b498ae43d44
gnm133 <gabrielnogueira67@gmail.com>
2022-12-04 21:26:05 -0300

    sanity comentado

--- sanity.c
+++ sanity.c
@@ -11,7 +11,7 @@ main(int argc, char *argv[])
  	}
 
 	int i, n, k, j = 0;
-	int retime;
+	int retime;		//Variavéis a serem monitoradas
 	int rutime;
 	int stime;
 	int record[3][3];
@@ -21,27 +21,27 @@ main(int argc, char *argv[])
 			record[i][j] = 0;
 
 	n = atoi(argv[1]);
-	n = n*3;
+	n = n*3; //3*N processos
 	int pid;
 
 	for (i = 0; i < n; i++){
 		j = i % 3;
 		pid = fork();
 		if (pid == 0) { //child
-			j = (getpid() - 4) % 3; // ensures independence from the first son's pid when gathering the results in the second part of the program
+			j = (getpid() - 4) % 3; //Garante indepência do pid do primeiro filho
 			switch(j){
-				case 0: //CPU‐bound process (CPU):
+				case 0: //Processos CPU‐bound (CPU):
 						for (double z = 0; z < 10000000.0; z+= 0.1){
-				         double x =  x + 3.14 * 89.64;   // useless calculations to consume CPU time
+				         double x =  x + 3.14 * 89.64;   // calculando tempo de uso da CPU
 					}
 					break;
-				case 1: //short tasks based CPU‐bound process (S‐CPU):
+				case 1: //Processos curtos CPU‐bound (S‐CPU):
 					for (k = 0; k < 100; k++){
 						for (j = 0; j < 1000000; j++){}
 						sys_yield();
 					}
 					break;
-				case 2:// simulate I/O bound process (IO)
+				case 2://Processos simulando I/O bound (IO)
 					for(k = 0; k < 100; k++){
 						sleep(1);
 					}
@@ -54,21 +54,21 @@ main(int argc, char *argv[])
 
 	for (i = 0; i < n; i++){
 		pid = wait2(&retime, &rutime, &stime);
-		int res = (pid - 4) % 3; // correlates to j in the dispatching loop
+		int res = (pid - 4) % 3;
 		switch(res) {
-			case 0: // CPU bound processes
+			case 0: // simulando processos CPU bound
 				printf(1, "CPU-bound, pid: %d, ready: %d, running: %d, sleeping: %d, turnaround: %d\n", pid, retime, rutime, stime, retime + rutime + stime);
 				record[0][0] += retime;
 				record[0][1] += rutime;
 				record[0][2] += stime;
 				break;
-			case 1: // CPU bound processes, short tasks
+			case 1: // simulando processos CPU bound (tarefas curtas)
 				printf(1, "SCPU bound, pid: %d, ready: %d, running: %d, sleeping: %d, turnaround: %d\n", pid, retime, rutime, stime, retime + rutime + stime);
 				record[1][0] += retime;
 				record[1][1] += rutime;
 				record[1][2] += stime;
 				break;
-			case 2: // simulating I/O bound processes
+			case 2: // simulando processos I/O bound
 				printf(1, "I/O bound, pid: %d, ready: %d, running: %d, sleeping: %d, turnaround: %d\n", pid, retime, rutime, stime, retime + rutime + stime);
 				record[2][0] += retime;
 				record[2][1] += rutime;
@@ -80,7 +80,8 @@ main(int argc, char *argv[])
 	for (i = 0; i < 3; i++)
 		for (j = 0; j < 3; j++)
 			record[i][j] /= n;
-
+	
+	//imprime a média de tempo para conclusão dos processos.
 	printf(1, "\nCPU BOUND - Average sleeping time: %d - Average ready time: %d - Average turnaround time: %d\n\n",  record[0][2], record[0][0], record[0][0] + record[0][1] + record[0][2]);
 	printf(1, "SCPU BOUND - Average sleeping time: %d - Average ready time: %d - Average turnaround time: %d\n\n", record[1][2], record[1][0],  record[1][0] + record[1][1] + record[1][2]);
 	printf(1, "I/O BOUND - Average sleeping time: %d - Average ready time: %d - Average turnaround time: %d\n\n", record[2][2], record[2][0],  record[2][0] + record[2][1] + record[2][2]);
